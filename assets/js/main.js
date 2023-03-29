662035451ef6742ac8a762b0ebb4d580ecdf5c91
Oriano <alfisti@rocketmail.com>
2023-03-29 10:38:13 +0200

    add  gravity fonctionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,25 +24,36 @@ let turn = 0;
 
 function play(elem) {
     let elemId = elem.id
+    let currentSymbol
+    let currentColor
     let index = parseInt(elemId.split("-")[1])   //question sur split
     console.log(gravityTable[index + 7]);
     if (elem.innerHTML == "" && elem.innerHTML == "" && gameOver == false) {
+        if (player % 2 != 0) {
+            currentColor = "red"
+            currentSymbol = "_"
+        } else {
+            currentColor = "yellow"
+            currentSymbol = "}"
+        }
+        elem.innerHTML = currentSymbol
+        elem.style.color = currentColor
         console.log(player);
-        for (let i = index; i < gravityTable.length - 1; i = i + 7) {
-            if (gravityTable[i + 7] && gravityTable[i + 7].innerHTML == "" && player % 2 != 0) {
-                gravityTable[i + 7].innerHTML = "_";
-                gravityTable[i].innerHTML = "";
-                console.log(player);
-            }
-            if (gravityTable[i + 7] && gravityTable[i + 7].innerHTML == "" && player % 2 == 0) {
-                gravityTable[i + 7].innerHTML = "}";
-                gravityTable[i].innerHTML = "";
-                console.log(player);
+        for (let i = index; i < gravityTable.length; i = i + 7) {
+            if (gravityTable[i] != "") {
+                if (gravityTable[i + 7] && gravityTable[i + 7].innerHTML == "") {
+                    gravityTable[i + 7].innerHTML = currentSymbol;
+                    gravityTable[i + 7].style.color = currentColor;
+                    gravityTable[i].innerHTML = "";
+                    elem.style.color = currentColor;
+                    console.log(player);
+                }
             }
         }
         player++
         updateGrid()
     }
+
 }
 
 
@@ -74,20 +85,24 @@ function checkWin() {
         for (let j = 0; j < checkWinTable[i].length; j++) {
             if (checkWinTable[i][j] != "" && checkWinTable[i][j] == checkWinTable[i][j + 1] && checkWinTable[i][j + 1] == checkWinTable[i][j + 2] && checkWinTable[i][j + 2] == checkWinTable[i][j + 3]) {
                 console.log("gagné");
+                gameOver = true;
             }
             if ((checkWinTable[i + 3] && checkWinTable[i][j] != "" && checkWinTable[i][j] == checkWinTable[i + 1][j] && checkWinTable[i + 1][j] == checkWinTable[i + 2][j] && checkWinTable[i + 2][j] == checkWinTable[i + 3][j])) {
                 console.log("gagné");
+                gameOver = true;
             }
             if ((checkWinTable[i + 3] && checkWinTable[i][j] != "" && checkWinTable[i][j] == checkWinTable[i + 1][j + 1] && checkWinTable[i + 1][j + 1] == checkWinTable[i + 2][j + 2] && checkWinTable[i + 2][j + 2] == checkWinTable[i + 3][j + 3])) {
                 console.log("gagné");
+                gameOver = true;
             }
             if ((checkWinTable[i + 3] && checkWinTable[i][j + 3] != "" && checkWinTable[i][j + 3] == checkWinTable[i + 1][j + 2] && checkWinTable[i + 1][j + 2] == checkWinTable[i + 2][j + 1] && checkWinTable[i + 2][j + 1] == checkWinTable[i + 3][j])) {
                 console.log("gagné");
-            } else {
-                if (turn > 41) {
-                    console.log("draw");
-                }
+                gameOver = true;
             }
         }
+    }
+    if (player >= 43 && gameOver == false) {
+        console.log("draw");
+        gameOver = true;
     }
 }
